ba77b7997ad9a2f9bdac0531bd751e54093d573f
marcoalexander <63327508+marcoalexander@users.noreply.github.com>
2022-04-13 19:29:26 -0500

    vertex class for graph building

---
+++ Vertex.java
@@ -0,0 +1,45 @@
+package com.globalsoftwaresupport.advancedalgorithms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Vertex{
+
+	private String name;
+	private boolean visited;
+	private List<Vertex> adjacencyList;
+
+	public Vertex(String name){
+		this.name = name;
+		this.adjacencyList = new ArrayList<>();
+	}
+
+	public boolean isVisited() {
+		return visited;
+	}
+
+	public void setVisited(boolean visited){
+		this.visited = visited;
+	}
+
+	public List<Vertex> getAdjacencyList(){
+		return adjacencyList;
+	}
+
+	public void addNeighbor(Vertex vertex){
+		this.adjacencyList.add(vertex);
+	}
+
+
+
+
+	@Override
+	public String toString(){
+		return this.name;
+	}
+
+
+
+
+
+}//end class
